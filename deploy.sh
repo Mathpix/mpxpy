83f347f3ffbcd3f14c6fb52497b7960ac15b0c1d
matt chang <matt@mathpix.com>
2025-05-15 10:42:26 -0400

    Ensure branch is on master before deploying to PyPi

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@
 # Script to build and upload mpxpy to PyPI
 
 set -e
+current_branch=$(git branch --show-current)
+if [[ "$current_branch" != "master" ]]; then
+    echo "ERROR: You must be on the master branch to deploy to PyPI. Current branch: $current_branch"
+    exit 1
+fi
 echo "DEPLOYING mpxpy TO OFFICIAL PyPI"
 read -p "Are you sure you want to proceed with deploying mpxpy to PyPI? (y/N) " -n 1 -r
 echo
